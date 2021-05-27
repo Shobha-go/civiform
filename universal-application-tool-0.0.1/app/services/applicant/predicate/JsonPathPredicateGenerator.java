24e8703da56a13de037a21c7c9b9c77a9e09b90a
Caroline Danzi <cdanzi@google.com>
2021-05-27 12:04:51 -0400

    Fetch generation and evaluation code from eval-predicate branch

--- universal-application-tool-0.0.1/app/services/applicant/predicate/JsonPathPredicateGenerator.java
+++ universal-application-tool-0.0.1/app/services/applicant/predicate/JsonPathPredicateGenerator.java
@@ -1,20 +1,45 @@
 package services.applicant.predicate;
 
-import services.Path;
+import static com.google.common.collect.ImmutableMap.toImmutableMap;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import services.applicant.exception.InvalidPredicateException;
+import services.applicant.question.ApplicantQuestion;
 import services.program.predicate.LeafOperationExpressionNode;
 
+/** Generates {@link JsonPathPredicate}s based on the current applicant filling out the program. */
 public class JsonPathPredicateGenerator {
+
+  private final ImmutableMap<Long, ApplicantQuestion> questionsById;
+
+  public JsonPathPredicateGenerator(ImmutableList<ApplicantQuestion> programQuestions) {
+    this.questionsById =
+        programQuestions.stream()
+            .collect(toImmutableMap(q -> q.getQuestionDefinition().getId(), q -> q));
+  }
 
   /**
    * Formats a {@link LeafOperationExpressionNode} in JsonPath format: {@code path[?(expression)]}
    *
    * <p>Example: \$.applicant.address[?(@.zip in ["12345", "56789"])]
    */
-  public JsonPathPredicate fromLeafNode(LeafOperationExpressionNode node, Path questionPath) {
+  public JsonPathPredicate fromLeafNode(LeafOperationExpressionNode node)
+      throws InvalidPredicateException {
+    if (!questionsById.containsKey(node.questionId())) {
+      // This means a predicate was incorrectly configured - we are depending upon a question that
+      // does not appear anywhere in this program.
+      throw new InvalidPredicateException(
+          String.format(
+              "Tried to apply a predicate based on question %d, which is not found in this"
+                  + " program.",
+              node.questionId()));
+    }
+
     return JsonPathPredicate.create(
         String.format(
             "%s[?(@.%s %s %s)]",
-            questionPath.predicateFormat(),
+            questionsById.get(node.questionId()).getContextualizedPath().predicateFormat(),
             node.scalar().name().toLowerCase(),
             node.operator().toJsonPathOperator(),
             node.comparedValue().value()));
--- universal-application-tool-0.0.1/test/services/applicant/predicate/JsonPathPredicateGeneratorTest.java
+++ universal-application-tool-0.0.1/test/services/applicant/predicate/JsonPathPredicateGeneratorTest.java
@@ -1,39 +1,70 @@
 package services.applicant.predicate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.google.common.collect.ImmutableList;
+import java.util.Optional;
+import org.junit.Before;
 import org.junit.Test;
 import services.Path;
 import services.applicant.ApplicantData;
+import services.applicant.exception.InvalidPredicateException;
+import services.applicant.question.ApplicantQuestion;
 import services.applicant.question.Scalar;
 import services.program.predicate.LeafOperationExpressionNode;
 import services.program.predicate.Operator;
 import services.program.predicate.PredicateValue;
+import services.question.types.QuestionDefinition;
+import support.TestQuestionBank;
 
 public class JsonPathPredicateGeneratorTest {
 
-  private final JsonPathPredicateGenerator generator = new JsonPathPredicateGenerator();
+  private QuestionDefinition question;
+  private JsonPathPredicateGenerator generator;
 
-  @Test
-  public void fromLeafNode_generatesCorrectFormat() {
-    LeafOperationExpressionNode node =
-        LeafOperationExpressionNode.create(
-            1L, Scalar.CITY, Operator.EQUAL_TO, PredicateValue.of("Seattle"));
-
-    assertThat(generator.fromLeafNode(node, Path.create("applicant.address")))
-        .isEqualTo(JsonPathPredicate.create("$.applicant.address[?(@.city == \"Seattle\")]"));
+  @Before
+  public void setupGenerator() {
+    TestQuestionBank questionBank = new TestQuestionBank(false);
+    question = questionBank.applicantAddress().getQuestionDefinition();
+    generator =
+        new JsonPathPredicateGenerator(
+            ImmutableList.of(
+                new ApplicantQuestion(question, new ApplicantData(), Optional.empty())));
   }
 
   @Test
-  public void fromLeafNode_canBeEvaluated() {
-    ApplicantData data = new ApplicantData();
-    data.putString(Path.create("applicant.address.city"), "Chicago");
+  public void fromLeafNode_generatesCorrectFormat() throws Exception {
     LeafOperationExpressionNode node =
         LeafOperationExpressionNode.create(
-            1L, Scalar.CITY, Operator.EQUAL_TO, PredicateValue.of("Chicago"));
+            question.getId(), Scalar.CITY, Operator.EQUAL_TO, PredicateValue.of("Seattle"));
 
-    JsonPathPredicate predicate = generator.fromLeafNode(node, Path.create("applicant.address"));
+    assertThat(generator.fromLeafNode(node))
+        .isEqualTo(
+            JsonPathPredicate.create("$.applicant.applicant_address[?(@.city == \"Seattle\")]"));
+  }
+
+  @Test
+  public void fromLeafNode_canBeEvaluated() throws Exception {
+    ApplicantData data = new ApplicantData();
+    data.putString(Path.create("applicant.applicant_address.city"), "Chicago");
+    LeafOperationExpressionNode node =
+        LeafOperationExpressionNode.create(
+            question.getId(), Scalar.CITY, Operator.EQUAL_TO, PredicateValue.of("Chicago"));
+
+    JsonPathPredicate predicate = generator.fromLeafNode(node);
 
     assertThat(data.evalPredicate(predicate)).isTrue();
+  }
+
+  @Test
+  public void fromLeafNode_missingQuestion_throwsInvalidPredicateException() {
+    LeafOperationExpressionNode node =
+        LeafOperationExpressionNode.create(123L, Scalar.DATE, Operator.IN, PredicateValue.of(23));
+
+    assertThatThrownBy(() -> generator.fromLeafNode(node))
+        .isInstanceOf(InvalidPredicateException.class)
+        .hasMessageContaining(
+            "Tried to apply a predicate based on question 123, which is not found in this program");
   }
 }
